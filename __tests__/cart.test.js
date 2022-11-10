6d62c54815d91befa85ee5485dedca8eed47d5cb
Keelen Fisher <keelencarrera@gmail.com>
2022-11-09 22:53:41 -0600

    cleaned tests

--- __tests__/cart.test.js
+++ __tests__/cart.test.js
@@ -14,14 +14,14 @@ describe('Cart Reducer', () => {
 
   it('can provide initial state', () => {
     let state = store.getState(); // snapshot of state AT THE MOMENT
-    expect(state.cart.productsInCart.length).toEqual(0);
-    expect(state.cart.productsInCart).toBeTruthy();
+    expect(state.cart.itemsForPurchase.length).toEqual(0);
+    expect(state.cart.itemsForPurchase).toBeTruthy();
   })
 
   it('can add products', () => {
     let state = store.getState(); // snapshot of state AT THE MOMENT
-    expect(state.cart.productsInCart.length).toEqual(0);
-    expect(state.cart.productsInCart).toBeTruthy();
+    expect(state.cart.itemsForPurchase.length).toEqual(0);
+    expect(state.cart.itemsForPurchase).toBeTruthy();
     let productOne = {name: 'product one', price: 5}
     let productTwo = {name: 'product two', price: 15}
 
@@ -29,9 +29,9 @@ describe('Cart Reducer', () => {
     store.dispatch(addItem(productTwo));
 
     state = store.getState(); // snapshot of state AT THE MOMENT
-    expect(state.cart.productsInCart.length).toEqual(2);
-    expect(state.cart.productsInCart[0].name).toEqual('product one');
-    expect(state.cart.productsInCart[1].name).toEqual('product two');
+    expect(state.cart.itemsForPurchase.length).toEqual(2);
+    expect(state.cart.itemsForPurchase[0].name).toEqual('product one');
+    expect(state.cart.itemsForPurchase[1].name).toEqual('product two');
   });
 
 });
